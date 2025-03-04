c7b4541f0f6e69b93738f1b094759e012680026f
quentalt <quentin.altieri@gmail.com>
2025-03-04 16:02:50 +0100

    refacto

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   template: `
     <footer class="bg-text text-white py-12 lg:py-16">
       <div class="container px-4">
